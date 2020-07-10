4de188abecbf43ea38b538b17748d46e0b0235b0
MMcClure11 <mcclme02@luther.edu>
2020-07-10 09:50:55 -0500

    adds uniq commentors method to post model and show page

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,17 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def commentors
+   self.comments.map { |comment| comment.user }.uniq { |user| user.username }
+  #  new_array = []
+  #   self.comments.each do |comment|
+  #     new_array << comment.user
+  #   end
+  #   new_array.uniq do |user|
+  #       user.username
+  #   end
+   
+  end
+
 
 end
