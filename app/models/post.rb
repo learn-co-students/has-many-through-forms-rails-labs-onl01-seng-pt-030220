6e115a46b7c69ace7541b2394ac529d25880168f
alelee93 <leevargasale@gmail.com>
2020-07-26 17:36:09 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,8 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :categories, reject_if: :all_blank
 
 
-  def comments_attributes=(comments_attributes) 
-    comments_attributes.values.each do |attr|
+  def comments_attributes=(comment_attributes) 
+    comment_attributes.values.each do |attr|
       comment = Comment.find_or_create_by(attr)
       self.comment_attributes.build(comment: comment) 
     end
