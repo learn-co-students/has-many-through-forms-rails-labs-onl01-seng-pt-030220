b68194cd155c91f4461cdd05b50283009e2b7c90
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-07-26 18:12:18 -0700

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,13 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(user_attributes)
+
+    if !user_attributes[:username].empty?
+      user = User.find_or_create_by(username: user_attributes[:username], email: user_attributes[:email])
+      user.comments << self
+    end
+
+  end
+
 end
